2565d01f3ee4ca3074af5354ae29106a1afe0c19
Jakeer <sjakeer244@gmail.com>
2023-08-29 13:18:02 +0000

    Solution

--- app.js
+++ app.js
@@ -24,6 +24,27 @@ const connectSeverDb = async () => {
 
 connectSeverDb();
 
+const logger = (request, response, next) => {
+  const header = request.headers["authorization"];
+  let jwtoken;
+  if (header !== undefined) {
+    jwtoken = header.split(" ")[1];
+  }
+  if (jwtoken === undefined) {
+    response.status(401);
+    response.send("Invalid JWT Token");
+  } else {
+    jwt.verify(jwtoken, "MY_KEY", async (error, user) => {
+      if (error) {
+        response.status(401);
+        response.send("Invalid JWT Token");
+      } else {
+        next();
+      }
+    });
+  }
+};
+
 app.post("/login/", async (request, response) => {
   const { username, password } = request.body;
   const query = `
@@ -49,3 +70,129 @@ app.post("/login/", async (request, response) => {
     }
   }
 });
+
+// GET states API
+
+const stateFormat = (i) => {
+  return {
+    stateId: i.state_id,
+    stateName: i.state_name,
+    population: i.population,
+  };
+};
+
+const districtFormat = (i) => {
+  return {
+    districtId: i.district_id,
+    districtName: i.district_name,
+    stateId: i.state_id,
+    cases: i.cases,
+    active: i.active,
+    deaths: i.deaths,
+  };
+};
+
+app.get("/states/", logger, async (request, response) => {
+  const query = `
+    select * from 
+       state`;
+
+  const dbresponse = await db.all(query);
+  let lst = [];
+  for (let i of dbresponse) {
+    const format = stateFormat(i);
+    lst.push(format);
+  }
+  response.send(lst);
+});
+
+// GET state with Id
+
+app.get("/states/:stateId/", logger, async (request, response) => {
+  const { stateId } = request.params;
+  const query = `
+  select * from state 
+  where 
+     state_id=${stateId};`;
+  const dbresponse = await db.get(query);
+  const format = stateFormat(dbresponse);
+  response.send(format);
+});
+
+// API create a district in a district table
+
+app.post("/districts/", logger, async (request, response) => {
+  const { districtName, stateId, cases, cured, active, deaths } = request.body;
+  const query = `
+  insert into district
+    (district_name,state_id,cases,cured,active,deaths)
+   values
+     ("${districtName}",${stateId},${cases},${cured},${active},${deaths});`;
+  const dbreesponse = await db.run(query);
+  response.send("District Successfully Added");
+});
+
+// API for getting district with districtId
+app.get("/districts/:districtId/", logger, async (request, response) => {
+  const { districtId } = request.params;
+  const query = `
+  select * from district
+  where district_id=${districtId};`;
+
+  const dbresponse = await db.get(query);
+  const format = districtFormat(dbresponse);
+  response.send(format);
+});
+
+// API delete district with respective districtID
+
+app.delete("/districts/:districtId/", logger, async (request, response) => {
+  const { districtId } = request.params;
+  const query = `
+  delete from district
+  where 
+     district_id=${districtId};`;
+  const dbresponse = await db.run(query);
+  response.send("District Removed");
+});
+
+// API update the district details
+
+app.put("/districts/:districtId/", logger, async (request, response) => {
+  const { districtId } = request.params;
+  const { districtName, stateId, cases, cured, active, deaths } = request.body;
+  const query = `
+  update 
+     district
+   set district_name="${districtName}",
+        state_id=${stateId},
+        cases=${cases},
+        cured=${cured},
+        active=${active},
+        deaths=${deaths}
+    where 
+       district_id=${districtId};`;
+
+  const dbrresponse = await db.run(query);
+  response.send("District Details Updated");
+});
+
+// API statics of state with stateId
+
+app.get("/states/:stateId/stats/", logger, async (request, response) => {
+  const { stateId } = request.params;
+  const query = `
+  select 
+     sum(cases) as totalCases,
+     sum(cured) as totalCured,
+     sum(active) as totalActive,
+     sum(deaths) as totalDeaths
+  from 
+     district 
+  where 
+     state_id=${stateId};`;
+  const dbresponse = await db.all(query);
+  response.send(...dbresponse);
+});
+
+module.exports = app;
